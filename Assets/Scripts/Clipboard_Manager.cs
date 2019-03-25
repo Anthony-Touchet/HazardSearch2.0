1ff18a97c6a024413d7a03f4f2906bac9f8fd88f
Anthony Touchet <anthonyjtouchet@gmail.com>
2019-03-25 17:05:45 -0500

    Clipboard Supscriptions

---
+++ Assets/Scripts/Clipboard_Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clipboard_Manager : MonoBehaviour
+{
+    public void ToggleOnOff(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    public void ToggleOnOff(Image image)
+    {
+        image.enabled = !image.enabled;
+    }
+
+    public void ToggleButton(Button button)
+    {
+        button.interactable = !button.interactable;
+    }
+}
