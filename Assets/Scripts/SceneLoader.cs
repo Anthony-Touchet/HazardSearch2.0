5473de56b9ae7184e6af9622c48ba39dcc1e4c83
@rootAccess <EricMouledoux@Gmail.com>
2019-03-26 09:09:00 -0500

    scene loader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string onLoadMessage;
+    
+    private Mouledoux.Components.Mediator.Subscriptions m_subscriptions =
+    new Mouledoux.Components.Mediator.Subscriptions();
+
+    public static SceneLoader _instance;
+
+    public SceneLoader instance
+    {
+        get
+        {
+            if (_instance == null) _instance = FindObjectOfType<SceneLoader>();
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetLoadMessage(string newMessage)
+    {
+        instance.onLoadMessage = newMessage;
+    }
+
+    private void OnLevelWasLoaded()
+    {
+        if (onLoadMessage == null) onLoadMessage = "";
+        
+        Mouledoux.Components.Mediator.instance.NotifySubscribers(onLoadMessage, new Mouledoux.Callback.Packet());
+        print(onLoadMessage);
+    }
+}
