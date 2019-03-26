0737deb289809d46c97aabf6e4f430f60b9b56f2
Anthony Touchet <anthonyjtouchet@gmail.com>
2019-03-26 15:29:39 -0500

    Audio Mixer Testing

---
+++ Assets/Scripts/SoundTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundTest : MonoBehaviour
+{
+    public AudioMixer m_audioMixer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void test(UnityEngine.UI.Slider slider)
+    {
+        m_audioMixer.SetFloat("Volume", slider.value);
+    }
+}
