a45225a08c1a5a05bc3cd2fe64fffb37c4908c79
Anthony Touchet <anthonyjtouchet@gmail.com>
2019-04-05 12:13:03 -0500

    Added tutorial Hazards

---
+++ Assets/Scripts/Animation Scripts/LerpScale.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpScale : MonoBehaviour
+{
+    public float m_lerpFactor;
+    public float m_speed;
+
+    private Vector3 origin;
+    private bool m_increasing;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_increasing = true;
+        origin = gameObject.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_increasing)
+            transform.localScale = 
+                new Vector3(transform.localScale.x + m_speed * Time.deltaTime,
+                    transform.localScale.y + m_speed * Time.deltaTime,
+                        transform.localScale.z + m_speed * Time.deltaTime);
+        else
+            transform.localScale = 
+                new Vector3(transform.localScale.x - m_speed * Time.deltaTime,
+                    transform.localScale.y - m_speed * Time.deltaTime,
+                        transform.localScale.z - m_speed * Time.deltaTime);
+        
+        if(transform.localScale.z >= origin.z + m_lerpFactor && m_increasing == true)
+            m_increasing = false;
+        else if(transform.localScale.z <= origin.z - m_lerpFactor && m_increasing == false)
+            m_increasing = true;
+    }
+}
---
+++ Assets/Scripts/Animation Scripts/RotateGameObjectOnYAxis.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGameObjectOnYAxis : MonoBehaviour
+{
+    public float m_speed;
+    public GameObject m_RotateHighlight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,m_speed*Time.deltaTime,0);
+        if(m_RotateHighlight != null)
+            m_RotateHighlight.transform.Rotate(0, m_speed*Time.deltaTime, 0);
+    }
+}
